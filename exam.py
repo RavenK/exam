943c1371ec9ad676cbced25cfd36087da1cfbb50
RAVEN <khue.phan.minh@gmail.com>
2016-02-19 00:39:05 +0100

    create a common function to select collumn from data list, create new function for plotting data within 5 , 15 mins. Removed select collumn part in cal_2nd_col

--- exam.py
+++ exam.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 in_file = open('C:\Users\User\Desktop\hrv\RR.rea')
 
 def find_invalid(file):
@@ -10,7 +11,6 @@ def find_invalid(file):
 			if i == "1":
 				invalid.append(count)
 	return invalid
-
 def select_column(file,column):
 	list_col= []
 	for lines in file:
@@ -21,32 +21,35 @@ def select_column(file,column):
 	list_col[0]=0
 	data_list = [float(i) for i in list_col]
 	return data_list
-def cal_2nd_col(file):
+def cal_2nd_col():
 	in_file.seek(0)
 	check = find_invalid(in_file)
 	in_file.seek(0)
 	data_list = select_column(in_file,1)
-
-	
 	for val in check:
 		data_list[val]=0
-		
 	max_value = max(data_list)
 	min_value = min(data_list)
 	average = np.mean(data_list)
 	standard_deviation= np.std(data_list)
 	print max_value,min_value,average,standard_deviation
-	#return data_list
+	return data_list
 	
-def interval_plot(data):
-	list_col_1= []
-	for lines in file:
-		line = lines.split()
-		if len(line) >1:
-			col_1 = line[0]
-			list_col_1.append(col_1)
-	
-a = cal_2nd_col(in_file)
+def interval_plot():
+	in_file.seek(0)
+	col_1 = select_column(in_file,0)
+	in_file.seek(0)
+	col_2 = select_column(in_file,1)
+	time_1 = 5*60 #5 mins to second
+	time_2 = 15*60 #15 mins to second
+	for i in col_1:
+		print i
+			
+	time = col_1[0:i]
+	plt.plot(time,col_2)
+	plt.show()
+			
+a = interval_plot()
 print a	
 
 			
